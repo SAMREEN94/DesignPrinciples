81a73b3e880678ce19fbd05fe391539b516f4ed1
SAMREEN94 <samreenfirdous94@mail.com>
2020-02-11 22:06:31 +0530

    first commit

---
+++ design/src/test/java/design/designprinciples.java
@@ -0,0 +1,14 @@
+package design;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class designprinciples {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
